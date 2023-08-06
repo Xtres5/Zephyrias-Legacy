73b80f537ee5222deb4c1696889d21d7daf57e15
winterishtuba <winterishtuba00@gmail.com>
2023-08-06 19:32:51 -0300

    Intentando obtener x,y de carta

--- core/src/com/clemente/zephyriaslegacy/Card.java
+++ core/src/com/clemente/zephyriaslegacy/Card.java
@@ -3,6 +3,7 @@ package com.clemente.zephyriaslegacy;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Stack;
@@ -18,25 +19,33 @@ public class Card {
 	public int cardHealth;
 	public int cardManaCost;
 	public Sprite cardFrame = new Sprite(new Texture("img/cardframe.png"));
-	public Sprite cardImage = new Sprite(new Texture("img/akali.jpg"));
-	private Table table;
-	private Stage stage;
-	private Skin skin;
+	public Sprite cardImage = new Sprite(new Texture("img/cardfront.png"));
+	private SpriteBatch batch;
+	private boolean selected;
+	private float x, y;
+//	private Table table;
+//	private Stage stage;
+//	private Skin skin;
 	
 	public Card(String cardName, String cardDescription, int cardDamage, int cardHealth, int cardManaCost, Sprite cardImage) {
-//		skin = new Skin(Gdx.files.internal("ui/glassy-ui.json"));
-//		stage = new Stage();
-//		table = new Table(skin);
-//		stage.addActor(table);
-//		table.setFillParent(false);
-		
+		this.batch = new SpriteBatch();
 		this.cardName = cardName;
 		this.cardDescription = cardDescription;
 		this.cardDamage = cardDamage;
 		this.cardHealth = cardHealth;
 		this.cardManaCost = cardManaCost;
 		this.cardImage = cardImage;
-//		
+		
+		
+		cardImage.setPosition(Gdx.graphics.getWidth() /2 - cardImage.getWidth() / 2, Gdx.graphics.getHeight() /2  - cardImage.getHeight() /2);
+		
+//		skin = new Skin(Gdx.files.internal("ui/glassy-ui.json"));
+//		stage = new Stage();
+//		table = new Table(skin);
+//		stage.addActor(table);
+//		table.setFillParent(false);
+		
+		
 //		table.add("cardManaCost"); 
 //		table.add(); 
 //		table.add("s"); //col3
@@ -73,9 +82,9 @@ public class Card {
 	}
 	
 	public void render() {
-//		stage.act();
-//		stage.draw();
-		cardImage.draw(Render.batch);
-		cardFrame.draw(Render.batch);
+		batch.begin();
+		batch.draw(cardImage, cardImage.getX(),  cardImage.getY());
+		batch.draw(cardFrame, cardFrame.getX(), cardFrame.getX());
+		batch.end();
 	}
 }
