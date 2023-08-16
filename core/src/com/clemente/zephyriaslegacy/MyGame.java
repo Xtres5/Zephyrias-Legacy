da1879dcca57526e536b04729bded2d10a51b1fa
winterishtuba <winterishtuba00@gmail.com>
2023-08-16 15:10:20 -0300

    Peter Griffin

--- core/src/com/clemente/zephyriaslegacy/MyGame.java
+++ core/src/com/clemente/zephyriaslegacy/MyGame.java
@@ -4,6 +4,8 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.graphics.Cursor;
+import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -20,6 +22,12 @@ public class MyGame extends Game {
 		this.batch = new SpriteBatch();
 //		Gdx.graphics.setFullscreenMode(Gdx.graphics.getDisplayMode());
 		this.setScreen(new TitleScreen(this));
+		Pixmap pixmap = new Pixmap(Gdx.files.internal("img/peter.png"));
+		// Set hotspot to the middle of it (0,0 would be the top-left corner)
+		int xHotspot = 15, yHotspot = 15;
+		Cursor cursor = Gdx.graphics.newCursor(pixmap, +xHotspot, yHotspot);
+		pixmap.dispose(); // We don't need the pixmap anymore
+		Gdx.graphics.setCursor(cursor);
 		
 	}
 
